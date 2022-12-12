af75733a659a0a2728d255cafd587cd9db18dc76
Melek Elloumi <melek.elloumi@insat.ucar.tn>
2022-12-12 10:08:58 +0100

    updated env

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -5,10 +5,13 @@ const environment = process.env.ENVIRONMENT;
 
 console.log('environment:::::', environment);
 
+require('dotenv').config()
+
+
 let ENVIRONMENT_VARIABLES = {
   'process.env.ENVIRONMENT': JSON.stringify('development'),
   'process.env.PORT': JSON.stringify('3080'),
-  'process.env.MONGO_CONNECTION_STRING': JSON.stringify('mongodb://mongo-tasks:x25keTrrfQF99i4OsNCSi2SbzxYqmTKtQkGoYEZKleJGeMtO9fEtWEdzXQ0zhkufLq7uNKevuCUKACDb7mBZtw==@mongo-tasks.mongo.cosmos.azure.com:10255/tasks?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@mongo-tasks@')
+  'process.env.MONGO_CONNECTION_STRING': JSON.stringify(process.env.AZURE_CONNECTION_STRING)
 };
 
 
